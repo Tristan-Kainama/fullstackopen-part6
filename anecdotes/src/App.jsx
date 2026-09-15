e7554f373c6174b4ea03c2db319c9166ec650f7c
baveryk <io.kainama@gmail.com>
2026-09-15 14:02:37 +0700

    Finished exercise 8

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -2,8 +2,15 @@ import { useAnecdotes, useAnecdotesActions } from "./store"
 import Filter from './components/Filter'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
+import { useEffect } from "react"
 
 const App = () => {
+  const { initialize } = useAnecdotesActions()
+
+  useEffect(() => {
+    initialize()
+  }, [initialize])
+
   return (
     <div>
       <Filter />
